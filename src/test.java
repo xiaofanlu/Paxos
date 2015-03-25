ce7dd86820bf80bcb4831d4d17b3c9ca77c08e53
xiaofanlu <lvxiaofan@gmail.com>
2015-03-25 10:17:25 -0500

    test 2

--- src/test.java
+++ src/test.java
@@ -1,8 +1,10 @@
 /**
  * Created by xiaofan on 3/25/15.
+ * fdas
  */
 public class test {
   public static void main(String[] args) {
     System.out.print("Hello World");
+    System.out.println("HAHA!");
   }
 }
